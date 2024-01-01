801cc1b57167a9ac9a655f0b73644696563ed751
gargmanik6080 <gargmanik6080@gmail.com>
2024-01-01 21:00:39 +0530

    Implemented POST, PUT and DELETE

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Note(models.Model):
 	title = models.CharField(max_length=200)
 	body = models.TextField()
-	# created_at = models.DateTimeField(auto_now_add=True)
+	created_at = models.DateTimeField(auto_now_add=True)
 	
-	def __str__(self):
-		# return self.title
-		return '%s %s' % (self.title, self.body)
+	# def __str__(self):
+	# 	return self.title
+		# return '%s %s' % (self.title, self.body)
--- api/resources.py
+++ api/resources.py
@@ -1,7 +1,10 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from api.models import Note
 
 class NoteResource(ModelResource):
     class Meta:
         queryset = Note.objects.all()
         resource_name = 'note'
+        authorization = Authorization()
+        fields = ['title', 'body']
